67a31c213122e6dd8ea633b23a4ef4ecefd28ce4
inininda <mawarnininda@gmail.com>
2026-08-14 14:54:08 +0700

    fix: changed middleware into proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     //return NextResponse.redirect(new URL("/hello", request.url))
     //if (request.nextUrl.pathname === '/profile') {
     //return NextResponse.redirect(new URL("/hello", request.nextUrl))
